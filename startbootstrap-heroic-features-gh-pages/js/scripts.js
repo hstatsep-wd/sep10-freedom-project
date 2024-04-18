fcd8f41539d7fb9e5c28c65e850174cf0103e720
simrans4258 <146861300+simrans4258@users.noreply.github.com>
2024-04-18 15:31:42 +0000

    Thu, Apr 18, 2024, 11:31 AM -04:00

--- startbootstrap-heroic-features-gh-pages/js/scripts.js
+++ startbootstrap-heroic-features-gh-pages/js/scripts.js
@@ -5,3 +5,8 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+$(document).ready(function(){
+    $("button").click(function(){
+        $(".fs-4").slideToggle("slow");
+    });
+});
