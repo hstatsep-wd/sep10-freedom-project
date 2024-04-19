6f6ddd31e5230bcb630970505cdc100899c24a47
simrans4258 <146861300+simrans4258@users.noreply.github.com>
2024-04-19 15:44:35 +0000

    Fri, Apr 19, 2024, 11:44 AM -04:00

--- startbootstrap-heroic-features-gh-pages/js/scripts.js
+++ startbootstrap-heroic-features-gh-pages/js/scripts.js
@@ -7,18 +7,18 @@
 // Use this file to add JavaScript to your project
 $(document).ready(function(){
     $("#button1").click(function(){
-        $("#thing1").toggle("slow");
+        $("#thing1").toggle("hide");
     });
 });
 
 $(document).ready(function(){
     $("#button2").click(function(){
-        $("#thing2").toggle("slow");
+        $("#thing2").toggle("hide");
     });
 });
 
 $(document).ready(function(){
     $("#button3").click(function(){
-        $("#thing3").toggle("slow");
+        $("#thing3").toggle("hide");
     });
 });
